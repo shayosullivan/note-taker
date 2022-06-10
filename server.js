8a6e5a170d0cfda32f7fc29ecf3810a405664eb5
shay <shayosullivan@hotmail.com>
2022-06-09 21:12:37 -0400

    routes created and server js made

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+const apiRoutes = require("./routes/apiRoutes");
+app.use(apiRoutes);
+const htmlRoutes = require("./routes/htmlRoutes");
+app.use(htmlRoutes);
+
+//create server listener
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
