ec748f5a1539fe96a886ab638469629b8623ae8d
shay <shayosullivan@hotmail.com>
2022-06-12 03:19:41 -0400

    dropped api and html.js couldnt figure it out

--- server.js
+++ server.js
@@ -1,16 +1,48 @@
+//const htmlRoutes = require("./routes/htmlRoutes");
+//const apiRoutes = require("./routes/apiRoutes");
+const fs = require("fs");
 const express = require("express");
-const htmlRoutes = require("./routes/htmlRoutes");
-const apiRoutes = require("./routes/apiRoutes");
+const notes = require("./db/db.json");
+const path = require("path");
+const uniqId = require("uniqid");
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use(apiRoutes);
-app.use(htmlRoutes);
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  let noteId = uniqId();
+  newNote.id = noteId;
 
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+  fs.readFile("./db/db.json", (err, notes) => {
+    if (err) throw err;
+    const notesArr = JSON.parse(notes);
+    notesArr.push(newNote);
+
+    fs.writeFile(
+      "./db/db.json",
+      JSON.stringify(notesArr, null, 2),
+      "utf8",
+      (err) => {
+        if (err) return console.log(err);
+        res.json(newNote);
+      }
+    );
+  });
+});
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./db/db.json"));
+});
